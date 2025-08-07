3b2305056305180f64c1301ceb7f7e03ad23e518
Shiva Priyan <pshanmugashiva@gmail.com>
2025-08-07 02:02:49 -0400

    Add files via upload

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -1,17 +1,17 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution(object):
     def hasCycle(self, head):
-        l = []
+        l = {}
         while head:
             if head.next in l:
                 return True
             else:
-                l.append(head)
+                l[head] = ''
                 head = head.next
         return False
         
