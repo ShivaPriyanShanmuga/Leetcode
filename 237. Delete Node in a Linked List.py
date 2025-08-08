e98fb61a4a2a9ab46a64aee343ada0e360d25fc8
Shiva Priyan <pshanmugashiva@gmail.com>
2025-08-08 00:28:36 -0400

    Add files via upload

---
+++ 237. Delete Node in a Linked List.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        cpy = node
+        while node.next.next:
+            node.val = node.next.val
+            node = node.next
+        node.val = node.next.val
+        node.next = None
