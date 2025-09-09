957057cc395478e5860cb96bbbe1ef136ecff463
Shiva Priyan <pshanmugashiva@gmail.com>
2025-09-09 19:16:12 -0400

    Add files via upload

---
+++ 1502. Can Make Arithmetic Progression From Sequence.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for i in range(len(arr) - 1):
+            if arr[i+1] - arr[i] == diff:
+                pass
+            else:
+                return False
+        return True
